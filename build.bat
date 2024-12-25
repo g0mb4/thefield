ecfc39c74bda562962dff754ccbe9b3a7b856ab1
g0mb4 <gomba007@gmail.com>
2024-12-25 19:29:00 +0100

    Add Windows build.

---
+++ build.bat
@@ -0,0 +1,15 @@
+echo off
+
+mkdir thefield 2> NUL
+mkdir thefield\maps 2> NUL
+
+cd src
+qcc
+copy "progs.dat" "..\thefield\progs.dat"
+cd ..
+
+cd map
+qbsp thefield.map thefield.bsp
+light thefield.bsp
+vis thefield.bsp
+copy "thefield.bsp" "..\thefield\maps\thefield.bsp"
