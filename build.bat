c2bb2dd432387e8dd260d96c97edbc1fd2f8c337
g0mb4 <gomba007@gmail.com>
2024-12-26 18:48:43 +0100

    Stop build.bat on error.

--- build.bat
+++ build.bat
@@ -4,13 +4,19 @@ mkdir thefield 2> NUL
 mkdir thefield\maps 2> NUL
 
 cd src
-qcc
-copy "progs.dat" "..\thefield\progs.dat"
+qcc || goto :error
+copy "progs.dat" "..\thefield\progs.dat" || goto :error
 cd ..
 
 cd map
-qbsp thefield.map thefield.bsp
-light thefield.bsp
-vis thefield.bsp
-copy "thefield.bsp" "..\thefield\maps\thefield.bsp"
+qbsp thefield.map thefield.bsp || goto :error
+light thefield.bsp || goto :error
+vis thefield.bsp || goto :error
+copy "thefield.bsp" "..\thefield\maps\thefield.bsp" || goto :error
 cd ..
+
+goto :EOF
+
+:error
+echo ---------- ERROR ----------
+exit /b 1
